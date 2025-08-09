c469d1f9185bc6a13b076b680748136173c32e6a
HShrigma <hristostantchev@abv.bg>
2025-08-09 21:05:56 +0300

    refactor[tweak]: Update RunnableTask to Task - Removes print

--- task-tracker/src/main/java/com/hshrigma/task_tracker/entity/Task.java
+++ task-tracker/src/main/java/com/hshrigma/task_tracker/entity/Task.java
@@ -1,17 +1,16 @@
 package com.hshrigma.task_tracker.entity;
 
-public class RunnableTask implements BaseTask {
+public class Task implements BaseTask {
     long id;
     String name;
     String description;
     Boolean completed;
 
-    public RunnableTask(long id, String name, String description) {
+    public Task(long id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
         completed = false;
-        run();
     }
 
     @Override
@@ -32,14 +31,5 @@ public class RunnableTask implements BaseTask {
     @Override
     public String getDescription() {
         return description;
-    }
-
-    public void run() {
-        StringBuilder message = new StringBuilder();
-        message.append("Task ID: ").append(getID())
-                .append("\n\tName: ").append(getName())
-                .append("\n\tDescription: ").append(getDescription())
-                .append("\n is running...");
-        System.out.println(message.toString());
     }
 }
--- task-tracker/src/main/java/com/hshrigma/task_tracker/repo/MockTaskRepository.java
+++ task-tracker/src/main/java/com/hshrigma/task_tracker/repo/MockTaskRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import com.hshrigma.task_tracker.entity.BaseTask;
-import com.hshrigma.task_tracker.entity.RunnableTask;
+import com.hshrigma.task_tracker.entity.Task;
 
 @Repository
 public class MockTaskRepository implements TaskRepository {
@@ -13,11 +13,11 @@ public class MockTaskRepository implements TaskRepository {
 
     public MockTaskRepository() {
         this.tasks = List.of(
-                new RunnableTask(1, "Clean", "Tidy up space"),
-                new RunnableTask(2, "Write Code", "Implement Task Service"),
-                new RunnableTask(3, "Walk Dog", "Get the dog out on a walk"),
-                new RunnableTask(4, "Cook", "Make some pasta"),
-                new RunnableTask(5, "Reward", "Chill out"));
+                new Task(1, "Clean", "Tidy up space"),
+                new Task(2, "Write Code", "Implement Task Service"),
+                new Task(3, "Walk Dog", "Get the dog out on a walk"),
+                new Task(4, "Cook", "Make some pasta"),
+                new Task(5, "Reward", "Chill out"));
     }
 
     @Override
